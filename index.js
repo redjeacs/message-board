f7d53bf2d9623376137a8cb3599044820ecf5eaa
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-10-19 15:38:48 +0800

    port change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
 
-const PORT = 8080;
+const PORT = 8000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
